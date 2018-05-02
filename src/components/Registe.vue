07754c5133fe25396e84a960c3c250e0f0d68a0e
danyuliu <35439500+danyuliu@users.noreply.github.com>
2018-05-02 18:07:47 +0800

    Registe

--- src/components/Registe.vue
+++ src/components/Registe.vue
@@ -4,8 +4,9 @@
       <el-col :span="16" :offset="4" id="head">
         <div class="title">Register</div>
         <div class="login-link" id="login-link">
-          <span>我已注册，现在就</span>
-          <el-button class="login-btn" id="login_btn" size="mini" @click="login">登录</el-button>
+          <span>I have registered,</span>
+          <el-button class="login-btn" id="login_btn" size="mini" @click="login">Login</el-button>
+          <span>now</span>
         </div>
       </el-col>
     </el-row>
@@ -16,15 +17,20 @@
             <el-input v-model="registeForm.userName" placeholder="Enter Username" clearable
              @focus="isShowNameTips=true" @blur="isShowNameTips=false"></el-input>
             <div v-if="isShowNameTips" class="focus-tips">
-              <span>Cannot be changed after setting.<br/>Up to 14 characters.</span>
+              <span>Cannot be changed after setting.<br/>5 to 10 characters in length.</span>
             </div>
           </el-form-item>
+
+          <el-form-item label="Email:" prop="email" class="registe-form-item">
+            <el-input v-model="registeForm.email" placeholder="Enter Email" clearable></el-input>
+          </el-form-item>
+
           <el-form-item label="Password:" prop="password" class="registe-form-item">
             <el-input type="password" v-model="registeForm.password" placeholder="Enter Password" clearable
-             @focus="isShowPwdTips=true" @blur="isShowPwdTips=false"></el-input>
+             @focus="isShowPwdTips=true" @blur="isShowPwdTips=true"></el-input>
             <ul v-if="isShowPwdTips" class="focus-tips-ul">
               <i class="el-icon-arrow-left pwd-checklist-arrow"/>
-              <li>6~14 characters in length.</li>
+              <li icon="el-icon-success">8~14 characters in length.</li>
               <li>Supports digits, uppercase and lowercase letters and punctuation.</li>
               <li>No spaces allowed.</li>
             </ul>
@@ -34,15 +40,18 @@
              @focus="isShowPwdTips2=true" @blur="isShowPwdTips2=false"></el-input>
             <ul v-if="isShowPwdTips2" class="focus-tips-ul">
               <i class="el-icon-arrow-left pwd-checklist-arrow"/>
-              <li>6~14 characters in length.</li>
+              <li>8~14 characters in length.</li>
               <li>Supports digits, uppercase and lowercase letters and punctuation.</li>
               <li>No spaces allowed.</li>
             </ul>
           </el-form-item>
-
-          <!-- brithday
-            remember
-          accept -->
+          <el-form-item label="Brithday" class="registe-form-item">
+            <el-date-picker type="date" placeholder="Select date" v-model="registeForm.brithday" style="width: 100%;"
+             :picker-options="brithdayOptions" :default-value="defaultBrithday"></el-date-picker>
+          </el-form-item>
+          <el-form-item class="registe-form-item">
+            <el-checkbox v-model="isRemember">Remember me next time</el-checkbox>
+          </el-form-item>
 
           <el-form-item class="registe-form-button">
             <el-button type="primary" @click="submitForm('registeForm')">Sign up</el-button>
@@ -59,33 +68,48 @@
       var validateConfirmPassword = (rule, value, callback) => {
         if(value != this.registeForm.password) {
           this.registeForm.confirmPassword = "";
-          callback(new Error("两次密码输入不一致"));
+          callback(new Error("The password and the confirmation password must be consistent."));
         } else {
           callback();
         }
       };
+      const date = new Date();
+      var atLeast18 = date.setFullYear(date.getFullYear() - 18);
       return {
         isShowNameTips: false,
         isShowPwdTips: false,
         isShowPwdTips2: false,
+        isRemember: false,
         registeForm: {
           userName: '',
+          email: '',
           password: '',
-          confirmPassword: ''
+          confirmPassword: '',
+          brithday: ''
+        },
+        defaultBrithday: atLeast18,
+        brithdayOptions: {
+          disabledDate(time) {
+            return time.getTime() > atLeast18;
+          }
         },
         rules: {
           userName: [
             { required: true, message: 'Please enter username', trigger: 'blur' },
-            { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' }
+            { min: 5, max: 10, message: '5 to 10 characters in length', trigger: 'blur' }
+          ],
+          email: [
+            { required: true, message: 'Please enter email', trigger: 'blur' },
+            { pattern: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/, message: 'Please enter the correct email address.', trigger: 'blur' }
           ],
           password: [
             { required: true, message: 'Please enter password', trigger: 'blur' },
-            { pattern: /^(\w){8,12}$/, message: '只能输入8-12个字母、数字、下划线', trigger: 'blur' }
+            { pattern: /^(\w){8,14}$/, message: '8~14 characters in length.Supports digits, uppercase and lowercase letters and punctuation.', trigger: 'blur' }
           ],
           confirmPassword: [
             { required: true, message: 'Please re-enter password', trigger: 'blur' },
-            { pattern: /^(\w){8,12}$/, message: '只能输入8-12个字母、数字、下划线', trigger: 'blur' },
-            { validator: validateConfirmPassword, trigger: 'blur'}
+            { pattern: /^(\w){8,14}$/, message: '8~14 characters in length.Supports digits, uppercase and lowercase letters and punctuation.', trigger: 'blur' },
+            { validator: validateConfirmPassword, trigger: 'blur' }
           ]
         }
       };
@@ -136,12 +160,12 @@
 }
 
 .registe-form-item {
-  margin-right: 270px;
+  margin: 0 270px 32px 0;
 }
 .registe-form label {
   width: 150px !important;
 }
-.registe-form label+div {
+.registe-form .el-form-item__content {
   margin-left: 150px !important;
 }
 
@@ -154,25 +178,24 @@
   color: #666;
   font-size: 12px;
   line-height: 20px;
-  text-align: left;
   margin-left: 10px;
 }
 .focus-tips-ul {
   height: inherit;
   margin: 0 0 0 20px;
-  padding: 5px 28px;
+  padding: 5px 0px 5px 24px;
   border: 1px solid #ddd;
   box-shadow: 1px 1px 1px #efefef;
   background: #f9f9f9;
+  list-style: none;
 }
 .focus-tips span {
   padding-top: 6px;
 }
 .focus-tips-ul li {
   line-height: 20px;
-  background: url(/static/passpc-account/img/reg/reg_icons.png) no-repeat -86px -112px;
+  background: url(/assets/reg/reg_icons.png) no-repeat -86px -112px;
   color: #666;
-  text-align: left;
 }
 .pwd-checklist-arrow {
   position: absolute;
@@ -182,7 +205,7 @@
 }
 
 .registe-form-button {
-  margin: 20px 270px 0 70px;
+  margin: 0 270px 0 70px;
 }
 .registe-form-button button{
   width: 100%;
